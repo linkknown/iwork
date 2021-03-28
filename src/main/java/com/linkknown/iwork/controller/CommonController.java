47ad039dcf707a42af4981d737d65c0626820ab3
389093982 <389093982@qq.com>
2021-03-28 13:49:35 +0800

    快捷 sql 界面支持 sql 格式化

--- src/main/java/com/linkknown/iwork/controller/CommonController.java
+++ src/main/java/com/linkknown/iwork/controller/CommonController.java
@@ -5,6 +5,8 @@ import com.github.pagehelper.PageInfo;
 import com.linkknown.iwork.adapter.PageAdapter;
 import com.linkknown.iwork.config.IworkConfig;
 import com.linkknown.iwork.entity.AppId;
+import com.linkknown.iwork.util.SqlFormatterUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,6 +30,18 @@ public class CommonController {
 
         resultMap.put("status", "SUCCESS");
         resultMap.put("EnvNameList", envNameList);
+
+        return resultMap;
+    }
+
+    @RequestMapping("/formatSql")
+    public Object formatSql (@RequestParam("sql") String sql) {
+        Map<String, Object> resultMap = new HashMap<>();
+
+        sql = SqlFormatterUtil.format(sql);
+
+        resultMap.put("status", "SUCCESS");
+        resultMap.put("sql", sql);
 
         return resultMap;
     }
